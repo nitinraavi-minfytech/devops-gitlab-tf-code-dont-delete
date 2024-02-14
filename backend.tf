011cfc558ab30c0d57da4939406fbdb9b74a3309
Ashish Sarawad <Ashish Sarawad>
2024-02-14 18:50:15 +0530

    Pushed tf code

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "devops-gitlab-tf-state-bucket-dont-delete"
-    key            = "devops-gitlab/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "devops-gitlab-state-locking-table-dont-delete"
-  }
-}
